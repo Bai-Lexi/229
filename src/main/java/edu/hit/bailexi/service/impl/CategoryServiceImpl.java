9e6742b11d9f3be2bfa51faa3583d22c6a010c6d
TsingruDong <kingrudong@gmail.com>
2023-06-29 09:27:54 +0800

    fix(category 排序):

--- src/main/java/edu/hit/bailexi/service/impl/CategoryServiceImpl.java
+++ src/main/java/edu/hit/bailexi/service/impl/CategoryServiceImpl.java
@@ -5,6 +5,7 @@ import edu.hit.bailexi.dao.CategoryDao;
 import edu.hit.bailexi.dao.impl.CategoryDaoImpl;
 import edu.hit.bailexi.service.CategoryService;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class CategoryServiceImpl implements CategoryService {
@@ -12,5 +13,9 @@ public class CategoryServiceImpl implements CategoryService {
     private CategoryDao categoryDao = new CategoryDaoImpl();
 
     @Override
-    public List<Category> findAll() { return categoryDao.findAll();}
+    public List<Category> findAll() {
+        List<Category> cs = categoryDao.findAll();
+        cs.sort(Comparator.comparingInt(Category::getCid));
+        return cs;
+    }
 }
