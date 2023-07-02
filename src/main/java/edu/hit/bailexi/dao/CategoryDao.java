94972178a62603233a7c5391111c1483a2e173e4
HaoYupengya <haoyupengya@163.com>
2023-07-02 17:10:31 +0800

    详情页面修改

--- src/main/java/edu/hit/bailexi/dao/CategoryDao.java
+++ src/main/java/edu/hit/bailexi/dao/CategoryDao.java
@@ -1,8 +1,11 @@
 package edu.hit.bailexi.dao;
 import edu.hit.bailexi.domain.Category;
+
 import java.util.List;
 
 public interface CategoryDao {
 
     public List<Category> findAll();
+
+    public Category findByCid(int cid);
 }
--- src/main/java/edu/hit/bailexi/dao/impl/CategoryDaoImpl.java
+++ src/main/java/edu/hit/bailexi/dao/impl/CategoryDaoImpl.java
@@ -1,8 +1,8 @@
 package edu.hit.bailexi.dao.impl;
 
 import edu.hit.bailexi.dao.CategoryDao;
-import edu.hit.bailexi.util.JDBCUtils;
 import edu.hit.bailexi.domain.Category;
+import edu.hit.bailexi.util.JDBCUtils;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 
@@ -15,5 +15,11 @@ public class CategoryDaoImpl implements CategoryDao {
     public List<Category> findAll() {
         String sql = "select * from tab_category";
         return  template.query(sql,new BeanPropertyRowMapper<Category>(Category.class));
+    }
+
+    @Override
+    public Category findByCid(int cid) {
+        String sql = "select * from tab_category where cid = ?";
+        return  template.queryForObject(sql,new BeanPropertyRowMapper<Category>(Category.class),cid);
     }
 }
